ea5f74353ef33fbcf5f349f44bcc5bafd6200de6
Keerti Dhanyaa <keertiramkumar@gmail.com>
2025-02-03 22:10:40 +0700

    Removed virtual environment from repo

--- backend/algorithms/search.py
+++ backend/algorithms/search.py
@@ -3,3 +3,15 @@ def linear_search(arr, target):
         if arr[i] == target:
             return i
     return -1
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
