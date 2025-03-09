e2fbc5b3441966648547d9dfe6cf05903b22eb3b
Richitha Elango <richi.elan@gmail.com>
2025-03-09 16:15:58 +0530

    added login code files

---
+++ frontend/login page/login.js
@@ -0,0 +1,69 @@
+
+import React from "react";
+import "./style.css";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+
+          <div className="text-wrapper">Login</div>
+
+          <div className="text-wrapper-2">Username</div>
+
+          <div className="rectangle-2" />
+
+          <div className="rectangle-3" />
+
+          <div className="text-wrapper-3">Enter email/username</div>
+
+          <div className="text-wrapper-4">Password</div>
+
+          <div className="text-wrapper-5">Enter password</div>
+
+          <div className="rectangle-4" />
+
+          <div className="text-wrapper-6">LOGIN</div>
+
+          <div className="text-wrapper-7">Create a new account?</div>
+
+          <div className="rectangle-5" />
+
+          <div className="text-wrapper-8">Sign up</div>
+        </div>
+
+        <div className="overlap-group">
+          <div className="group">
+            <div className="overlap-2">
+              <div className="overlap-wrapper">
+                <div className="overlap-3">
+                  <div className="overlap-group-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-9">AlgoRize</div>
+                    </div>
+                  </div>
+
+                  <p className="see-the-logic-master">
+                    <span className="span">
+                      <br />
+                    </span>
+
+                    <span className="text-wrapper-10">
+                      See the Logic, Master the Code!
+                    </span>
+                  </p>
+                </div>
+              </div>
+
+              <div className="ellipse" />
+            </div>
+          </div>
+
+          <div className="group-2" />
+        </div>
+      </div>
+    </div>
+  );
+};
