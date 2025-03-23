e48b38b98f2b9a710bf369acfeeb71c46c0a9675
Richitha Elango <richi.elan@gmail.com>
2025-03-23 11:02:46 +0530

    edited all settings header sidebar help and community

--- frontend/src/header/header.jsx
+++ frontend/src/header/header.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import menuIcon from "../assets/menu.png";
 import profilePic from "../assets/female-user.png";
 import "./header.css";
 
-const Header = ({ toggleSidebar }) => {
+const Header = () => {
   const navigate = useNavigate();
 
   return (
     <header className="header">
-      <button className="menu-btn" onClick={toggleSidebar}>
-        <img src={menuIcon} alt="Menu" />
-      </button>
       <div className="logo">AlgoRize</div>
-      <button className="profile-btn" onClick={() => navigate("/settings")}>
-        <img src={profilePic} alt="Profile" />
-      </button>
+      <div className="header-right">
+        <button className="profile-btn" onClick={() => navigate("/settings")}>
+          <img src={profilePic} alt="Profile" />
+        </button>
+      </div>
     </header>
   );
 };
