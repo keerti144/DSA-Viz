22fca975ba22353dd8255c6ea3bd7bc22201bb0c
Srinithi0605 <ssrinithi2005@gmail.com>
2025-05-19 15:43:25 +0530

    community

--- frontend/src/homepage/homepage.jsx
+++ frontend/src/homepage/homepage.jsx
@@ -35,17 +35,6 @@ export const Homepage = () => {
       <Header />
 
       <div className="homepage-main-content">
-        {/* 
-        <div className="search-container">
-          <br/>
-          <br/>
-          <br/>
-          <input type="text" className="search-input" placeholder="Search topics..." />
-          <img src={search} alt="Search" className="search-icon" />
-        </div>
-        <br/>
-        <br/>
-        <br/>
         <h1 className="welcome-text">WELCOME TO ALGORIZE</h1>
 
         <div className="intro-section">
